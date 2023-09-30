aa2545f3529d85542e6f4f29eeac26e4f8454642
miguel alonso <migalonso@deloitte.com>
2023-09-30 14:13:12 -0600

    Excercise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+	public static void main (String[] args) {
+
+		// este codigo debe compilar
+		/*
+		Persona madre = new Persona.Builder("Maria")
+                .setMunicipio("Selva")
+                .setMayor(37)
+                .setLugarTrabajo("Google")
+                .build();
+
+		Persona hijo = new Persona.Builder("Pedro")
+                .setMenor(4)
+                .setColegio("Colegio de Selva")
+                .build();
+		*/
+
+
+		// esta codigo NO debe compilar
+		/*
+		Persona mal = new Persona.Builder("Luisa")
+				    .setMayor(20)
+				    .setColegio("Colegio de Villa Arriba")
+				    .build();
+        */
+	}
 }
