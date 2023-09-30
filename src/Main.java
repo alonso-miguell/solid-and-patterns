991c301098662fcd95224d6a8bccd3ffa1134aca
miguel alonso <migalonso@deloitte.com>
2023-09-30 14:16:18 -0600

    Solution

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
 	public static void main (String[] args) {
 
 		// este codigo debe compilar
-		/*
 		Persona madre = new Persona.Builder("Maria")
                 .setMunicipio("Selva")
                 .setMayor(37)
@@ -14,15 +13,16 @@ public class Main {
                 .setMenor(4)
                 .setColegio("Colegio de Selva")
                 .build();
-		*/
-
 
 		// esta codigo NO debe compilar
-		/*
+
 		Persona mal = new Persona.Builder("Luisa")
 				    .setMayor(20)
 				    .setColegio("Colegio de Villa Arriba")
 				    .build();
-        */
+
+
+
 	}
+
 }
--- src/Persona.java
+++ src/Persona.java
@@ -22,20 +22,25 @@ public class Persona {
 			return this;
 		}
 		
-		public Builder setMayor(int edad, String lugarTrabajo) {
+//		public BuilderMayor setMayor(int edad, String lugarTrabajo) {
+		public BuilderMayor setMayor(int edad) {
 			if (edad < 18) throw new IllegalArgumentException("es menor de edad " + edad);
 			persona.edad = edad;
-			persona.lugarTrabajo = lugarTrabajo;
-			persona.colegio = null;
-			return this;
+
+//			persona.lugarTrabajo = lugarTrabajo;
+//			persona.colegio = null;
+//			return this;
+
+			return new BuilderMayor(persona);
 		}
 		
-		public Builder setMenor(int edad, String colegio) {
+		public BuilderMenor setMenor(int edad) {
 			if (edad >= 18) throw new IllegalArgumentException("es mayor de edad " + edad);
 			persona.edad = edad;
-			persona.colegio = colegio;
-			persona.lugarTrabajo = null;
-			return this;
+
+//			persona.colegio = colegio;
+//			persona.lugarTrabajo = null;
+			return new BuilderMenor(persona);
 		}
 	
 		public Persona build() {
@@ -43,5 +48,47 @@ public class Persona {
 		}
 
 	}
-	
+
+
+	/*
+	BUIlders agregados, Mayor y menor
+	Al no tener setColegio en BuilderMayor, fallara en tiempo de compilacion.
+	De la misma manera al no tener un metodo setLuharTrabajo fallara en BuilderMenor
+	 */
+
+	public static class BuilderMenor {
+		private Persona persona;
+
+		public BuilderMenor(Persona persona){
+			this.persona=persona;
+		}
+
+		public BuilderMenor setColegio(String colegio){
+			persona.colegio=colegio;
+			return this;
+		}
+
+		public Persona build(){
+			return persona;
+		}
+	}
+
+	public static class BuilderMayor {
+		private Persona persona;
+
+		public BuilderMayor(Persona persona){
+			this.persona=persona;
+		}
+
+		public BuilderMayor setLugarTrabajo(String lugarTrabajo){
+			persona.lugarTrabajo=lugarTrabajo;
+			return this;
+		}
+
+		public Persona build(){
+			return persona;
+		}
+	}
+
+
 }
