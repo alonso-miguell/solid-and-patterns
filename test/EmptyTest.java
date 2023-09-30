20a67b2ab1516e8d7e687c625116bd4d3097880c
miguel alonso <migalonso@deloitte.com>
2023-09-30 13:09:19 -0600

    Test commit

---
+++ test/EmptyTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import org.junit.Assert;
+
+public class EmptyTest {
+
+    @Test
+    public void empty_test(){
+        Assert.assertEquals("empty","empty");
+
+
+    }
+
+
+}
