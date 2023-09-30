c33e72b4b2c304336c5447b3fe8096c0fd453319
miguel alonso <migalonso@deloitte.com>
2023-09-30 13:20:53 -0600

    Problem to solve

---
+++ test/liskov/TestPuntos.java
@@ -0,0 +1,29 @@
+package liskov;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TestPuntos {
+
+	private final Punto2D referencia = new Punto2D(3,5);
+	private final List<Punto2D> puntos_prueba = Arrays.asList(
+			                                         new Punto2D(2,3), 
+			                                         new Punto2D(3,5), 
+			                                         new Punto2D(4,3)
+			                                         );
+	
+	
+	@Test
+	public void test_iguales_si_solo_si_distancia_0() {
+		
+		for (Punto2D punto: puntos_prueba) {
+			Assert.assertEquals("test para " + referencia + " y " + punto,
+					             referencia.equals(punto), 
+					             referencia.distancia(punto) == 0);
+		}
+	}
+
+}
